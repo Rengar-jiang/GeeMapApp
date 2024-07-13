211138ab1022c0461ee52a0ce81fc28ce6ae817c
姜田亮 <>
2024-07-13 10:48:56 +0800

    authenticate2

--- pages/nlcd_demo.py
+++ pages/nlcd_demo.py
@@ -7,7 +7,6 @@ import os
 # geemap.set_proxy(7890)
 # 从环境变量中读取token
 ## geemap.set_proxy( 60145,'http://us0150.alibabaokz.com', timeout=10)
-ee.Authenticate()
 def getNLCD(year):
     # Import the NLCD collection.
     dataset = ee.ImageCollection("USGS/NLCD_RELEASES/2019_REL/NLCD")
